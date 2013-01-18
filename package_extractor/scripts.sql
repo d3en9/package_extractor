55660799a03883ead3a708e23cd15bf03d73703d
d3en9 <efmahalin@gmail.com>
2013-01-18 17:47:58 +0400

    fix sql

--- package_extractor/scripts.sql
+++ package_extractor/scripts.sql
@@ -11,15 +11,15 @@
 		            info.reportyear = '2012' and 
 		            info.quarter = '3';
 
-select model.id, model.urldocument FROM "поручениеобработкиотчетности" as model 
+select model.id, model.urldocument, info.reportmonth, info.reportday, report.creationDate FROM "поручениеобработкиотчетности" as model 
 	            inner join reportpackage report on
 		            model.reportpackageid = report.id 
 	            inner join информацияопакете info on
 		            model.packageinfoid = info.id
 	            where
 		            model.актуальныйСтатус = 'IncomingNumberAssotiated' and
-		            info.rootname like 'ПакетСООУРЦБ1408%' and
+		            info.rootname like 'ПакетСООУРЦБ1409%' and
 		            info.modelname = 'PURCB' 
 		            and
-		            info.reportyear = '2012' and 
-		            info.quarter = '3'
+		            info.reportmonth = '12' and
+		            info.reportyear = '2012';
